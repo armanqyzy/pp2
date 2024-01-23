0f9df879a67ed1dded12232e792b39fe7d0091f1
Armankyzy Anara <156772378+armanqyzy@users.noreply.github.com>
2024-01-23 15:44:08 +0600

    Add files via upload
    LAB 1

---
+++ lab1/numbers.py
@@ -0,0 +1,19 @@
+# №1 Insert the correct syntax to convert x into a floating point number.
+
+x = 5
+x=float(x)
+
+# №2 Insert the correct syntax to convert x into a integer.
+
+x=5.5
+x=int(x)
+
+# №3 Insert the correct syntax to convert x into a complex number.
+
+x=5
+x=complex(x)
+
+#Return the string without any whitespace at the beginning or the end.
+
+txt = " Hello World "
+x=txt.strip()
